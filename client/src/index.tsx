cfd4824163eff1827286dbc35c6a0be28b085b64
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-03-23 13:29:20 -0700

    feat(bootstrap): added bootstrap to client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './custom.scss';
 import './index.scss';
 
 ReactDOM.render(
