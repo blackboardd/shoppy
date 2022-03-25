38e6a3698af97edc08c0eff0c6935538bd3cce1c
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-03-25 16:50:40 -0700

    feat(model\product\BookProduct): book implementation of the abstract product class

---
+++ server/model/BookProduct.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Abstract class for product objects.
+ *
+ * PHP version 8.1
+ *
+ * LICENSE: MIT
+ *
+ * @category   Product
+ * @package    model\product
+ * @author     Brighten Tompkins <brightenqtompkins@gmail.com>
+ * @copyright  2022 Brighten Tompkins
+ * @license    https://opensource.org/licenses/MIT MIT
+ */
+
+namespace model\product;
+
+include_once 'AbstractProduct.php';
+include_once 'SymbolInterface.php';
+
+// {{{ constants
+
+/**
+ * Enum for weight symbols.
+ */
+enum Weight implements SymbolInterface
+{
+  case KILOGRAMS;
+  case POUNDS;
+
+
+  /**
+   * Get the symbol for the weight.
+   */
+  public function getSymbol(): string
+  {
+    return match ($this) {
+      /**
+       * kilograms
+       */
+      Weight::KILOGRAMS => 'kg',
+
+      /**
+       * pounds
+       */
+      Weight::POUNDS => 'lbs',
+    };
+  }
+}
+
+// }}}
+// {{{ BookProduct
+
+/**
+ * Class for book product objects.
+ */
+class BookProduct extends AbstractProduct
+{
+  // {{{ properties
+
+  /**
+   * Weight of the product.
+   * 
+   * @var string
+   */
+  public $weight;
+
+  /**
+   * unit of the product.
+   * 
+   * @var Weight
+   */
+  public $unit;
+
+  public function __construct($id, $sku, $name, $price, $type, $weight)
+  {
+    parent::__construct($id, $sku, $name, $price, $type);
+    $this->weight = $weight;
+    $this->unit = new Weight('KILOGRAMS');
+  }
+
+  /**
+   * Set the unit of the product.
+   * 
+   * @param string $unit
+   *  The unit of the product.
+   * 
+   * @return void
+   * @access public
+   */
+  public function setUnit($unit): void
+  {
+    $this->unit = new Weight($unit);
+  }
+
+  /**
+   * Get the content of the product.
+   * 
+   * @return string
+   * @access public
+   */
+  public function getContent(): string
+  {
+    return "
+    {$this->sku}
+    {$this->name}
+    {$this->currency}{$this->price}
+    {$this->type}: {$this->weight}{$this->unit}
+    ";
+  }
+}
